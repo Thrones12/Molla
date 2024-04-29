a4a1a866c889c004fd2cd009230271e4d8619bf3
hungphong <hungphong.congviec@gmail.com>
2024-04-29 11:51:06 +0700

    feat: check out v1

--- Molla/src/main/java/DaiHoc/Molla/controller/web/CheckOutController.java
+++ Molla/src/main/java/DaiHoc/Molla/controller/web/CheckOutController.java
@@ -2,14 +2,18 @@ package DaiHoc.Molla.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class CheckOutController {
 	@GetMapping("checkout")
-	public String getDetail() {
-		//${review.user.fullname}
+	public String getCheckout() {
 		return "web/views/checkout";
+	}
+	@PostMapping("checkout")
+	public String postCheckout() {
+		return "web/views/thank";
 	}
 }
